ab34dc925a17497770bd5262d4b610adf6f0834c
VShershnov <vshershnyov@gmail.com>
2016-06-06 00:41:46 +0300

    P182Rectangular finished RectParalpd.java,  modify Tester.java

--- P182Rectangular/RectParalpd.java
+++ P182Rectangular/RectParalpd.java
@@ -3,9 +3,9 @@ package part2.practice.P182Rectangular;
 import java.util.Random;
 
 public class RectParalpd {
-	int high;
-	int length;
-	int wide;
+	private int high;
+	private int length;
+	private int wide;
 	
 	RectParalpd(){
 		Random rnd = new Random();
@@ -14,22 +14,95 @@ public class RectParalpd {
 		wide = rnd.nextInt(200);
 	}
 	
+	private void emptyDataMsg(int l, int w, int h){
+		String s = "Сторони паралепіпеда не можуть бути 0 або відємними. Додайте корректно ";
+		
+		if (l<=0)
+			s += "довжину основи ";
+		if (w<=0) 
+			s += "ширину основи ";
+		if (h<=0)
+			s += "висоту";
+		
+		System.out.println(s);
+		
+	}
+	
 	RectParalpd(int l, int w){
-		high = 0; 		length = l; 	wide = w;
-		System.out.println("Створено прямокутник: length = " + length + ", wide = " + wide);
+		if (l*w>0){
+			high = 0; 		length = l; 	wide = w;
+			System.out.println("Створено прямокутник: length = " + length + ", wide = " + wide);
+			System.out.println("Щоб отрімати паралепіпед, додайте висоту");
+		}
+		else 
+			emptyDataMsg(l, w, 1);
 	}
 	
 	RectParalpd(int l, int w, int h){
+		emptyDataMsg(l, w, h);
 		high = h; 		length = l; 	wide = w;
 	}
 	
+	public int surface (int i){ // 1 - h+w; 2 - h+l; 3 - l+w
+		switch (i) {
+	    case 1: 
+		    return high*wide;
+		case 2: 
+			return high*length;
+		case 3:		    
+		default: 
+			return length*wide;
+			
+		}
+	}
 	
+	public int fullSurface(){
+		if (high*wide*length>0)
+			return 2*(surface(1) + surface(2) + surface(3));
+		else { 
+			emptyDataMsg(length, wide, high);
+			return 0;
+		}
+	}
+	
+	public int volume(){
+		if (high*wide*length>0)
+			return high*wide*length;
+		else { 
+			emptyDataMsg(length, wide, high);
+			return 0;
+		}
+	}
 	
 
 	@Override
 	public String toString() {
 		return "RectParalpd [high=" + high + ", length=" + length + ", wide="
 				+ wide + "]";
+	}
+
+	public int getHigh() {
+		return high;
+	}
+
+	public void setHigh(int high) {
+		this.high = high;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getWide() {
+		return wide;
+	}
+
+	public void setWide(int wide) {
+		this.wide = wide;
 	}
 	
 	
