fa3d36ec3898b0933911f62136688f7d435a9f47
VShershnov <vshershnyov@gmail.com>
2016-06-06 21:09:17 +0300

    abstract P20 Car, Door, Engine, SportCar, Tester, Wheel 	new file:   "\320\235\320\276\320\262\321\213\320\271 \321\202\320\265\320\272\321\201\321\202\320\276\320\262\321\213\320\271 \320\264\320\276\320\272\321\203\320\274\320\265\320\275\321\202.txt"

---
+++ P20/Tester.java
@@ -0,0 +1,19 @@
+package part2.practice.P20;
+
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SportCar sc = new SportCar();
+		System.out.println(sc.toString());
+		
+		sc.installWheels();
+		sc.openDoors();
+		sc.startEngine();
+		System.out.println(sc.toString());
+		
+	}
+
+}
